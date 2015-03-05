2a4db84f11bfa80b7a573c8207dda7c1d66416a9
Steve Pothier <pothiers@gmail.com>
2015-03-05 12:27:09 -0700

    snap

--- fitscore/fitcurve.py
+++ fitscore/fitcurve.py
@@ -11,12 +11,13 @@ import csv
 import scipy.optimize
 import numpy
 import os.path
-import itertools
+import itertools as it
+import decimal
 
 import fitscore as fs
 
 def tt_swim():
-    fn = os.path.expanduser('~/sandbox/fitscore/data/swim-dtp.csv')
+    fn = os.path.expanduser('~/sandbox/fitscore/daata/swim-dtp.csv')
     popt = get_params(fn,swim_func, 2.378)
     print('val={}'.format(calc_value(swim_func, 1, 30, popt)))
     return popt
@@ -42,11 +43,10 @@ def swim_func(dt, c1, c2):
     (d,t) = dt
     return c1*d*d/t - c2
 
-# In [177]: fitcurve.tt_bike()
 # D,T shape: (2, 172)
-# perr=[ 0.10698979  1.40342227]
-# val=74.4832646726
-# Out[177]: array([ 15.40226164,  10.22917434])
+# perr=[ 0.10574954  1.38545338]
+# val=74.60815965908817
+# array([ 15.42056158,  10.20492902])
 def bike_func(dt, c1,c2):
     (d,t) = dt
     return c1*d*d/t - c2
@@ -62,6 +62,7 @@ def run_func(dt, c1, c2):
     return c1*d*d/t - c2
  
 
+
 # should use mask
 def get_data(fname,maxRate):
     '''maxRate :: max reasonable rate (mph) for activity'''
@@ -72,7 +73,8 @@ def get_data(fname,maxRate):
         reader = csv.DictReader(csvfile)
         for row in reader:
             if row['P'] == '': continue
-            d0, d1,t0,t1,p = [float(row[k]) for k in ['D0','D1','T0','T1','P']]
+            d0, d1,t0,t1,p = [decimal.Decimal(row[k])
+                              for k in ['D0','D1','T0','T1','P']]
             if d0 == 0: continue
             if t0 < 0.1:
                  t0 = 60*d0/maxRate
@@ -87,7 +89,8 @@ def apply_func(fname, func):
     with open(fname) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            d0, d1,t0,t1,p = [float(row[k]) for k in ['D0','D1','T0','T1','P']]
+            d0, d1,t0,t1,p = [decimal.Decimal(row[k])
+                              for k in ['D0','D1','T0','T1','P']]
             if p == 0: continue
             if t0 == 0: continue
             p2 = func(d0, t0) * 30/100
@@ -118,84 +121,144 @@ def duration(minutes):
     return('{:02d}:{:02d}:{:02d}'.format(h,m,s))
 
 
-def read_expected(fname):
+def read_expected(csvfilename):
+    if csvfilename ==  None:
+        return dict()
     expected = dict() # dict['{d0},{d1},{t0},{t1}'] = pscore
-    with open(fname) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            if row['P'] == '': continue
-            d0, d1,t0,t1,p = [float(row[k]) for k in ['D0','D1','T0','T1','P']]
+    keys = list()
+    with open(csvfilename, newline='') as csvfile:
+        for row in csv.reader(csvfile, strict=True):
+            if row[4] == '': continue
+            if row[4] == 'P': continue
+            d0, d1,t0,t1,p = [decimal.Decimal(n) for n in row]
             if d0 == 0: continue
-            key = ('{:.1f}\t{:.1f}\t{}\t{}'
-                   .format(d0,d1,duration(t0),duration(t1)))
+            #!key = ('{:.1f}\t{:.1f}\t{:.1f}\t{:.1f}'.format(d0,d1,t0,t1))
+            key = (round(d0,1), round(d1,1), round(t0,1), round(t1,1))
             expected[key] = p
-    return expected
+            keys.append(key)
+    return expected,keys
 
 
-def run_results(expected=None, results=None):
-    print('\nResults for: RUN (d0, d1, t0, t1, fscore)')
-    bigV = 12.0 # fasted speed to score (mph)
-    bigT = 9*60 # longest time running (minutes)
+def print_results(ordered, results, expected):
+    #!missing_expected = set(expected.keys()) - set(results.keys())
+    #!missing_results = set(results.keys()) - set(expected.keys())
+    #!print('Missing expected {}/{}'
+    #!      .format(len(missing_expected), len(expected),))
+    #!print('Missing results {}/{}'
+    #!      .format(len(missing_results),  len(results),))
+    #!
+    #!print ('Expected keys: {}'.format(sorted(expected.keys())[0:4]))
+    #!print ('Results  keys: {}'.format(sorted(results.keys())[0:4]))
+
+    #!print('Missing: Expected(d0,d1,t0,t1)  |       Results(d0,d1,t0,t1)')
+    #!if True:
+    #!    missed_e = sorted(missing_expected)
+    #!    missed_r = sorted(missing_results)
+    #!else:
+    #!    missed_e = sorted(expected)
+    #!    missed_r = sorted(results)
+    #!for idx in range(min(70,len(missed_e))):
+    #!    data = missed_e[idx] + missed_r[idx]
+    #!    print('{:5.1f} {:5.1f} {:5.1f} {:5.1f}  | {:5.1f} {:5.1f} {:5.1f} {:5.1f}'.format(*data))
+
+
+    prev_d0 = None
+    print('{:>5s} {:>5s}  {:>5s} {:>5s} | {:>6s}  |  {:>6s}'
+          .format('D0','D1', 'T0', 'T1', 'Expect', 'Actual'))
+    for key in ordered:
+        d0,d1,t0,t1 = key
+        if prev_d0 != d0:
+            print()
+        print('{:5.1f} {:5.1f}  {:5.1f} {:5.1f} | {:6.2f}  |  {:6.2f}'
+              .format(d0,
+                      d1,
+                      t0,
+                      t1,
+                      expected[key],
+                      results[key]*30/100,
+                  ))
+        prev_d0 = d0
+
+def gen_index(activity):
+    index_list = list()
+    if activity == 'run':
+        bigV = 12.0 # fasted speed to score (mph)
+        bigT = 9*60 # longest time active (minutes)
+        # distance (=miles x 10)
+        ll0 = [d/10.0 for d in it.chain(range(10,50),
+                                        range(50,100,5),
+                                        range(100,280,10))]
+
+        for d0,d1 in it.zip_longest(ll0[:-1], it.islice(ll0,1,None)):
+            d = d0
+            if d < 1.1:        
+                v_list = [3, 4, 5, 6, 7.5, 9, 10.5, bigV]     #slowest mph
+            if 1.1 <= d < 2:        
+                v_list = [2, 3, 4, 5, 6, 7.5, 9, 10.5, bigV]  #slowest mph
+            else:
+                v_list = [3, 4, 5, 6, 7.5, 9, 10.5, bigV]     
+            min_t = [60*d/v for v in v_list]  # minutes
+            max_t = [bigT] + [t for t in min_t[:-1]]
+            for (t0,t1) in zip(min_t, max_t):
+                index_list.append((decimal.Decimal(round(d0,1)),
+                                   decimal.Decimal(round(d1,1)),
+                                   decimal.Decimal(round(t0,1)),
+                                   decimal.Decimal(round(t1,1))
+                               ))
+    elif activity == 'bike':
+        pass
+    elif activity == 'swim':
+        pass
+    else:
+        error('Unknown activity: {}'.format(activity))
+
+    return index_list
+    
+def run_results(expected_file=None, results_file=None):
+    print('\nResults for RUN')
+    if expected_file == None:
+        keys = gen_index('run')
+    else:
+        expected,keys = read_expected(expected_file)
+
+    results = dict() # results['{d0},{d1},{t0},{t1}'] = fscore
+    for key in keys:
+        (d0,d1,t0,t1) = key
+        fscore = fs.run_fs(float(d0), float(t0))
+        results[key] = fscore
+        score = 30*fscore/100
+    print_results(keys, results, expected)
+    return results
+        
+def bike_results(expected=None, results=None):
+    print('\nResults for: BIKE')
     results = dict() # results['{d0},{d1},{t0},{t1}'] = fscore
     ordered = list()
+    bigV = 25.0 # fasted speed to score (mph)
+    bigT = 11*60 # longest time active (minutes)
 
-    # distance (=miles x 10)
-    ll0 = [d/10 for d in itertools.chain(range(10,50),
-                                         range(50,100,5),
-                                         range(100,280,10))]
-    
-    for d0,d1 in itertools.zip_longest(ll0[:-1], itertools.islice(ll0,1,None)):
+    ll0 = list(it.chain(range(2,30), range(30,105,5)))
+    for d0,d1 in it.zip_longest(ll0[:-1], it.islice(ll0,1,None)):
         d = d0
-        # slowest speeds: 0, 2, 3, 4, 5, 6, 7.5, 9, 10.5 
-        # slowest speeds: 0,    3, 4, 5, 6, 7.5, 9, 10.5 (>= 2 miles)
-        # max time (=seconds)
-        if d < 2:        
-            v_list = [2, 3, 4, 5, 6, 7.5, 9, 10.5, bigV]  #mph
-        else:
-            v_list = [3, 4, 5, 6, 7.5, 9, 10.5, bigV]     
-        min_t = [60*d/v for v in v_list]  # minutes
+        v_list = [10,15,20, bigV]
+        min_t = [60*d/v for v in v_list]
         max_t = [bigT] + [t for t in min_t[:-1]]
         #!print()
-        for (t0,t1) in zip(min_t, max_t):
+        for (t0, t1) in zip(min_t, max_t):
             fscore = fs.run_fs(d, t0)
             key = ('{:.1f}\t{:.1f}\t{}\t{}'
                    .format(d0,d1,duration(t0),duration(t1)))
             results[key] = fscore
             score = 30*fscore/100
             ordered.append(key)
-            #!print('{}\t{}\t{}\t{}\t{:.2f}'
-            #!      .format(d0, d1, duration(t0), duration(t1), score))
-    prev_d0 = None
-    for key in ordered:
-        d0,d1,t0,t1 = key.split()
-        if prev_d0 != d0:
-            print()
-        print('{:5.1f}  {:5.1f}  {}  {}   {:6.2f}  {:6.2f}'
-              .format(float(d0),
-                      float(d1),
-                      t0,
-                      t1,
-                      results[key],
-                      results[key]*30/100,
-                  ))
-        prev_d0 = d0
-        
-def bike_results(expected=None, results=None):
-    print('\nResults for: BIKE')
-    big = 1e9
-    for d in itertools.chain(range(2,30), range(30,105,5)):
-        v_list = [10,15,20, big]
-        min_t = [60*d/v for v in v_list]
-        max_t = [big] + [t for t in min_t[:-1]]
-        print()
-        for (min,max) in zip(min_t, max_t):
-            print('{}\t{}\t{}'.format(d, duration(min), duration(max)))
+            #! print('{}\t{}\t{}'.format(d, duration(min), duration(max)))
+    print_results(ordered, results)
 
 def swim_results(expected=None, results=None):
     print('\nResults for: SWIM')
     big = 1e9
     # distance (=yards)
-    for d in itertools.chain(range(200,1900,50), range(1900,3001,100)):
+    for d in it.chain(range(200,1900,50), range(1900,3001,100)):
         v_list = [1800, 2400, 3600, big] # yards/hour
         min_t = [60*d/v for v in v_list]
         max_t = [big] + [t for t in min_t[:-1]]
@@ -241,8 +304,11 @@ def main():
     logging.debug('Debug output is enabled!!!')
 
 
+    if args.expected:
+        args.expected.close()
+        args.expected = args.expected.name
     if args.activity == 'run':
-        run_results(expected=args.expected, results=args.out)
+        run_results(expected_file=args.expected, results_file=args.out)
     elif args.activity == 'swim':
         swim_results(expected=args.expected, results=args.out)
     elif args.activity == 'bike':
